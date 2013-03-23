b0f694c2463cd663674fbaa7b18dd1be0a6884cc
bruce_d <insertanyname@live.com>
2013-03-23 12:32:51 -0300

    Create thermo.py
    Intelligent Agent class file which controls the raspberry pi Thermostat
    and maintains an appropriate temperature based on the values passed to it and the membership functions defined in therm.fcl which is a Fuzzy Control Language file

---
+++ backend/lib/thermo.py
@@ -0,0 +1,78 @@
+import control
+import RPi.GPIO as GPIO
+import subprocess
+from datetime import datetime
+import re
+import fuzzball
+import time
+import threading
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+
+class Thermostat:
+    def __init__(self, pin, f_name, active):
+        self.control = control.Raspi(GPIO, pin, active)
+        self.input = Input(f_name)
+
+    def get_temp(self):
+        self.input.open_file()
+        self.input.read_file()
+        self.input.close_file()
+        self.data = self.input.get_data().split()[-1]
+        self.ret_val = float(self.data[2:])
+        self.ret_val = self.ret_val / 1000
+        return self.ret_val
+
+    def get_time(self):
+        self.time = datetime.now()
+        return self.time.hour
+        
+    def get_outside(self):
+        self.trial = subprocess.Popen(
+            "curl -s http://www.weatheroffice.gc.ca/city/pages/ns-35_metric_e.html | grep temperature ", stdout=subprocess.PIPE, shell=True).stdout.read()
+        return re.sub('[a-z><"=  \n]', '', self.trial)
+
+    def turn_on(self):
+        self.control.turn_on()
+
+    def turn_off(self):
+        self.control.turn_off()
+        
+class Input:
+    def __init__(self, f_name):
+        self.f_name = f_name
+
+    def setfile(self, f_name):
+        self.f_name = f_name
+
+    def open_file(self):
+        try:
+            self.tfile = open(self.f_name)
+        except IOError:
+            print ("Unable to Open File " + self.f_name)
+            
+    def read_file(self):
+        try:
+            self.temp_data = self.tfile.read()
+        except IOError:
+            print ("Unable to Read File " + self.f_name)
+    def close_file(self):
+        self.tfile.close()
+
+    def get_data(self):
+        return self.temp_data
+
+
+
+class ThermoThread(threading.Thread):
+    def run(self):
+        x = Thermostat(17, "/sys/bus/w1/devices/28-00000354a500/w1_slave", "LOW")
+        f = fuzzball.FuzzyTemp("lib/therm.fcl")
+        while True:
+            if f.calc_val(x.get_temp(), x.get_time(), x.get_outside()):
+                x.turn_on()
+            else:
+                x.turn_off()
+                time.sleep(60 * 5)
+
