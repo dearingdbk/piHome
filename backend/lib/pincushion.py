bf9a073f014cbca9d86d0b80f5eace726f73ad54
bruce_d <insertanyname@live.com>
2013-03-26 11:31:00 -0300

    Update pincushion.py

--- backend/lib/pincushion.py
+++ backend/lib/pincushion.py
@@ -16,16 +16,3 @@ class Cushion:
         if '_' + str(pin) in self.pin_dict:
             del self.pin_dict['_' + str(pin)]
 
-def main():
-    x = Cushion()
-    x.add_pin(23, "LOW")
-    x.add_pin(24, "HIGH")
-    x.get_val(23).turn_on()
-    print x.get_val(23).get_val()
-    x.get_val(23).turn_off()
-    print x.get_val(23).get_val()
-    print x.pin_dict
-    x.del_pin(23)
-    print x.pin_dict
-
-if __name__ == '__main__':main()
