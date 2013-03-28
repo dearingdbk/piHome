f2efcb58421f50f08dbf90739d55193183d6ffd8
dearingdbk <insertanyname@live.com>
2013-03-28 11:37:54 -0300

    Changed name of Database from todos to pinRegister

--- backend/code.py
+++ backend/code.py
@@ -1,18 +1,16 @@
-"""
- * File:     code.py
- * Author:   
- * Date:     2013/03/23
- * Version:  1.0
- *
- * Purpose:  Starts and handles calls/posts/requests to the webservice.
-"""
-import web          # web.py import # 
-import model        # handles calls to the database #
-import lib.thermo   # intelligent agent for climate control #
+""" Basic todo list using webpy 0.3 """
+import web
+import model
+import lib.thermo
+import lib.pincushion
 
+"""cushion = lib.pincushion.Cushion()
+pins = model.get_todos()
+for pin in pins:
+    cushion.add_pin(pin.id, pin.active)
 
-t = lib.thermo.ThermoThread()   # create the thread to manage thermostat #
-t.start()                       # start the thread #
+t = lib.thermo.ThermoThread()
+t.start()"""
 
 
 ### Url mappings
@@ -36,7 +34,7 @@ class Index:
         web.form.Dropdown('id', ['4', '17', '18', '21',
                                 '22', '23', '24', '25'],
                          description="Pin:"),
-        web.form.Dropdown('active', [('0', 'Low'), ('1', 'High')], 
+        web.form.Dropdown('active', [('LOW', 'Low'), ('HIGH', 'High')], 
                           description="Active State:"),
         web.form.Button('Add Device'),
         
@@ -44,17 +42,17 @@ class Index:
 
     def GET(self):
         """ Show page """
-        todos = model.get_todos()
+        pins = model.get_pins()
         form = self.form()
-        return render.index(todos, form)
+        return render.index(pins, form)
 
     def POST(self):
         """ Add new entry """
         form = self.form()
         if not form.validates():
-            todos = model.get_todos()
-            return render.index(todos, form)
-        model.new_todo(form.d.title, form.d.room, form.d.id, form.d.active)
+            pins = model.get_pins()
+            return render.index(pins, form)
+        model.new_pin(form.d.title, form.d.room, form.d.id, form.d.active)
         raise web.seeother('/')
 
 
@@ -64,7 +62,7 @@ class Delete:
     def POST(self, id):
         """ Delete based on ID """
         id = int(id)
-        model.del_todo(id)
+        model.del_pin(id)
         raise web.seeother('/')
 
 
@@ -72,3 +70,58 @@ app = web.application(urls, globals())
 
 if __name__ == '__main__':
     app.run()
+
+
+
+#import web
+#import view, config
+#from view import render
+
+#db = web.database(dbn="sqlite", db='pinRegister')
+
+#urls = (
+ #   '/', 'index'
+#)
+
+#class index:
+ #   def GET(self):
+  #      print db.query("23").list()
+   #     todos = db.select('*')
+    #    return render.index(todos)
+      # return render.base(view.listing())
+
+#if __name__ == "__main__":
+#    app = web.application(urls, globals())
+#    app.internalerror = web.debugerror
+#    app.run()
+#import web
+#import RPi.GPIO as GPIO
+#import view, config
+#from view import render
+#from subprocess import call
+
+#GPIO.setmode(GPIO.BCM)
+
+
+#urls = (
+#     '/(.*)', 'index'
+#)
+
+#class index:
+ #   def GET(self, pin):
+#        GPIO.setup(17, GPIO.OUT)
+#        if pin in ('off'):
+#            GPIO.output(17, GPIO.LOW)
+#        else:
+#            GPIO.output(17, GPIO.HIGH)
+        #call("echo " + pin + " > /sys/class/gpio/export", shell=True)
+        #call("echo 1 " + " > /sys/class/gpio/gpio" + pin + "/active_low", shell=True)
+        #call("echo \"out\" " + " > /sys/class/gpio/gpio" + pin + "/direction", shell=True)
+        #call("echo 0 " + " > /sys/class/gpio/gpio" + pin + "/value", shell=True)
+#        return render.index(GPIO.input(17))
+
+
+#if __name__ == "__main__":
+#    app = web.application(urls, globals())
+#    app.internalerror = web.debugerror
+#    app.run()
--- backend/lib/__init__.py
+++ backend/lib/__init__.py
@@ -1,32 +1,15 @@
 #!/usr/bin/env python
-"""web.py: makes web apps (http://webpy.org)"""
+"""piHome ~ Home Automation"""
+
 
 from __future__ import generators
 
-__version__ = "0.37"
+__version__ = "0.10"
 __author__ = [
-    "Aaron Swartz <me@aaronsw.com>",
-    "Anand Chitipothu <anandology@gmail.com>"
+    "Bruce Dearing",
+    "Samuel Coleman"
+    "Hua Chen"
 ]
 __license__ = "public domain"
-__contributors__ = "see http://webpy.org/changes"
+__contributors__ = "NIL"
 
-import utils, db, net, wsgi, http, webapi, httpserver, debugerror
-import template, form
-
-import session
-
-from utils import *
-from db import *
-from net import *
-from wsgi import *
-from http import *
-from webapi import *
-from httpserver import *
-from debugerror import *
-from application import *
-from browser import *
-try:
-    import webopenid as openid
-except ImportError:
-    pass # requires openid module
