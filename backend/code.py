8c5584450f4e579935e47e7b075460f08585c336
bruce_d <insertanyname@live.com>
2013-04-26 19:45:55 -0300

    Removed old url mappings and class definitions

--- backend/code.py
+++ backend/code.py
@@ -16,70 +16,7 @@ urls = (
     '/devices/(\d+)?', 'Devices',
     '/rooms/(\d+)?', 'Rooms'
 )
-"""
-    '/', 'Index',
-    '/del/(\d+)', 'Delete',
-    '/on/(\d+)', 'On',
-    '/off/(\d+)', 'Off'
-"""
 
-
-"""
-### Templates
-render = web.template.render('templates', base='base')
-
-class Index:
-
-    form = web.form.Form(
-        web.form.Textbox('title', web.form.notnull, 
-                         description="Description:"),
-        web.form.Textbox('room', web.form.notnull, 
-                         description="Location:"),
-        web.form.Dropdown('id', ['4', '17', '18', '21',
-                                '22', '23', '24', '25'],
-                         description="Pin:"),
-        web.form.Dropdown('active', [('LOW', 'Low'), ('HIGH', 'High')], 
-                          description="Active State:"),
-        web.form.Button('Add Device'),
-        
-    )
-
-    def GET(self):
-        "" Show page ""
-        pins = model.Device.get_pins()
-        form = self.form()
-        return render.index(pins, form)
-
-    def POST(self):
-        "" Add new entry ""
-        form = self.form()
-        if not form.validates():
-            pins = model.Device.get_pins()
-            return render.index(pins, form)
-        try:
-            model.Device.new_pin(form.d.title, form.d.room, form.d.id, form.d.active)
-            raise web.seeother('/')
-        except:
-            return render.error()
-
-class Delete:
-    def POST(self, id):
-        "" Delete based on ID ""
-        id = int(id)
-        model.Device.del_pin(id)
-        raise web.seeother('/')
-
-class On:
-    def POST(self, id):
-        id = int(id)
-        model.Device.turn_on(id)
-        raise web.seeother('/')
-
-class Off:
-    def POST(self, id):
-        model.Device.turn_off(id)
-        raise web.seeother('/')
-"""
 
 class Index:
     def GET(self):
